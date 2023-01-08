1f6f5bec10d8d996ac57f0d887260f5606a7ae16
pilipaleksandr <pilipenko.sasha2422@gmail.com>
2023-01-08 18:48:43 +0200

    Second commit

--- Task1.py
+++ Task1.py
@@ -28,6 +28,26 @@ class Rational:
     def show_float_form(self):
         print("Floating-point format of given number: ", self.__numerator / self.__denominator)
 
+    def add(self, obj):
+        return (self.__numerator / self.__denominator) + (obj.__numerator / obj.__denominator)
+
+    def sub(self, obj):
+        return (self.__numerator / self.__denominator) - (obj.__numerator / obj.__denominator)
+
+    def mul(self, obj):
+        return (self.__numerator / self.__denominator) * (obj.__numerator / obj.__denominator)
+
+    def div(self, obj):
+        return (self.__numerator / self.__denominator) / (obj.__numerator / obj.__denominator)
+
+    def comp(self, obj):
+        if (self.__numerator / self.__denominator) > (obj.__numerator / obj.__denominator):
+            print(self.__numerator / self.__denominator, ">", obj.__numerator / obj.__denominator)
+        elif (self.__numerator / self.__denominator) < (obj.__numerator / obj.__denominator):
+            print(self.__numerator / self.__denominator, "<", obj.__numerator / obj.__denominator)
+        else:
+            print(self.__numerator / self.__denominator, " = ", obj.__numerator / obj.__denominator)
+
 
 if __name__ == '__main__':
     try:
@@ -38,5 +58,14 @@ if __name__ == '__main__':
         obj2 = Rational(2, 5)
         obj2.show_rational_form()
         obj2.show_float_form()
+
+        print(obj1.add(obj2))
+        print(obj1.sub(obj2))
+        print(obj1.mul(obj2))
+        print(obj1.div(obj2))
+        obj1.comp(obj2)
+
+        obj3 = Rational(2, 5)
+        obj2.comp(obj3)
     except Exception as e:
         print(e)
